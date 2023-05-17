084d79ce7e498c0d86cd46720ff2ac631296fea9
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-05-17 20:40:15 +0700

    update

--- src/components/Forget.js
+++ src/components/Forget.js
@@ -46,24 +46,11 @@ const Forget=()=>{
             <button
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               type="submit"
-              onSubmit={handleSubmit}
+              onClick={handleSubmit}
             >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                      <svg
-                        className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12zm-3-6.5a1.5 1.5 0 113 0v1a1.5 1.5 0 01-3 0v-1zm3-3a1.5 1.5 0 110 3h-1a1.5 1.5 0 110-3h1z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </span>
-              Đặt lại mật khẩu
+
+                  Đặt lại mật khẩu
+
             </button>
           </form>
         </div>
