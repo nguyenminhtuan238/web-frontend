b424bbf186b273cbcc824da2b78508d3a066486f
NguyenMinhTuan <minhdu238@gmail.com>
2023-05-17 10:12:20 +0700

    prettier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import {BrowserRouter,Route,Routes} from 'react-router-dom'  
-import Home from './components';
-import Forget from './components/Forget';
-import Login from './components/Login';
-import Register from './components/register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components'
+import Forget from './components/Forget'
+import Login from './components/Login'
+import Register from './components/register'
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Home/>}></Route>
-              <Route path='/Login' element={<Login/>}></Route>
-              <Route path='/Register' element={<Register/>}></Route>
-              <Route path='/Forget' element={<Forget/>}></Route>
-            </Routes>
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/Login" element={<Login />}></Route>
+                    <Route path="/Register" element={<Register />}></Route>
+                    <Route path="/Forget" element={<Forget />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
-export default App;
+export default App
