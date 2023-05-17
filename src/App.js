fdbb84d5d623bf0c760b392b780dd601e363701a
Nhat-Duy <nhatduy1471ag@gmail.com>
2023-05-17 10:30:07 +0700

    Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from './components';
 import Forget from './components/Forget';
 import Login from './components/Login';
 import Register from './components/register';
+import Footer from './components/footer';
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
             <Routes>
               <Route path='/' element={<Home/>}></Route>
+              <Route path='/Footer' element={<Footer/>}></Route>
               <Route path='/Login' element={<Login/>}></Route>
               <Route path='/Register' element={<Register/>}></Route>
               <Route path='/Forget' element={<Forget/>}></Route>
