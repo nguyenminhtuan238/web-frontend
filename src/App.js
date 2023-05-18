59e514a535cf18ba10005a9f20ba3603abd83bf8
NguyenMinhTuan <minhdu238@gmail.com>
2023-05-18 15:52:24 +0700

    update 5/18 3:52

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import Home from './components';
 import Forget from './components/Forget';
 import Login from './components/Login';
 import Register from './components/register';
-import Footer from './components/footer';
-import Header from './components/header';
+import Layout from './components/layout/layout';
+import NotFound from './components/Notfound';
 function App() {
   return (
     <div className="App">
       <SnackbarProvider maxSnack={5}>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/Footer" element={<Footer />}></Route>
-            <Route path="/Header" element={<Header />}></Route>
-            <Route path="/Login" element={ <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+            <Route path="/" element={<Layout/>}>
+            <Route index element={<Home />}></Route>
+            <Route path="Login" element={ <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
                       <Login />
                 </SnackbarProvider>}>
               </Route>
-            <Route path="/Register" element={<Register />}></Route>
-            <Route path="/Forget" element={<Forget />}></Route>
+            <Route path="Register" element={<Register />}></Route>
+            <Route path="Forget" element={<Forget />}></Route>
+            </Route>
+            <Route path="*" element={<NotFound/>}></Route>
           </Routes>
         </BrowserRouter>
       </SnackbarProvider>
---
+++ src/components/layout/layout.js
@@ -0,0 +1,13 @@
+import Footer from '../footer';
+import Header from '../header';
+import { Outlet } from "react-router-dom";    
+const Layout=()=>{
+    return(
+        <div>
+            <Header/>
+               <Outlet/> 
+            <Footer/>
+        </div>
+    )
+}
+export default Layout
