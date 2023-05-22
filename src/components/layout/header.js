61a3b22d11f5c6e6e251d6a3831b6b81f20220ca
NguyenMinhTuan <minhdu238@gmail.com>
2023-05-22 09:36:02 +0700

    UPdate

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -6,7 +6,7 @@ function Header() {
   const [modal, setmodal] = useState(false);
   const [isSearch, setIsSearch] = useState(false);
   return (
-    <div className="w-full fixed">
+    <div className="w-full sticky top-0">
       <header>
         <nav className="bg-black border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 z-50">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
@@ -97,7 +97,7 @@ function Header() {
         />
       )}
       {isVisible && (
-        <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 border-b-4 border-indigo-500 animate-slideDown z-40 bg-white">
+        <div className="max-w-screen-2xl absolute  mx-auto px-4 sm:px-6 lg:px-8 border-b-4 border-indigo-500  animate-slideDown z-40 bg-white">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="col-span-1 ">
               <ul className="text-gray-400 mt-4 cursor-pointer ">
@@ -121,7 +121,8 @@ function Header() {
         </div>
       )}
       {isSearch &&(
-          <div className=" w-[640px] bg-gray-100 animate min-h-screen ml-auto absolute top-0 right-0 z-50 shadow-lg " style={{boxShadow: '0 0 1000px rgba(0, 0, 0,0.5)'}}>
+          <div className="min-h-screen bg-gray-100 flex flex-col justify-center   py-12 sm:px-6 lg:px-8 fixed inset-0  bg-opacity-30 backdrop-blur-sm z-50  ">
+          <div className=" w-[640px] bg-gray-100  animate-slideRight min-h-screen ml-auto  absolute top-0 right-0 z-50 shadow-lg " style={{boxShadow: '0 0 1000px rgba(0, 0, 0,0.5)'}}>
             <div className="absolute top-[0px] right-[0px] modal bg-gray-100 p-8">
               <Link onClick={() => setIsSearch(!isSearch)} >close</Link>  
             </div>
@@ -143,6 +144,7 @@ function Header() {
                 </button>
               </form>
             </div>
+          </div>
           </div>
       )}
     </div>
