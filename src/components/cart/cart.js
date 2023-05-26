27667ddbff4526da520f22f8c0b64e5f950a52cf
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-05-26 16:27:12 +0700

    update

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CartPage = () => {
   const [count, setCount] = useState(1);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const decreaseCount = () => {
     if (count > 1) {
       setCount(count - 1);
@@ -18,26 +29,26 @@ const CartPage = () => {
   };
 
   return (
-    <div className="grid grid-cols-3">
-      <div className="col-span-2 ml-16 mr-16">
-        <div className="text-lg font-bold mt-11 mb-10 ml-2">
+    <div className="grid grid-cols-1 lg:grid-cols-3">
+      <div className="lg:col-span-2 lg:ml-16 lg:mr-16">
+        <div className="text-lg font-bold mt-6 mb-4 lg:mt-11 lg:mb-10 lg:ml-2">
           <h2>Giỏ hàng</h2>
         </div>
 
-        <div className="bg-white rounded-lg p-4 shadow-2xl   border-2 ">
+        <div className="bg-white rounded-lg p-4 shadow-2xl border-2">
           <table className="min-w-full divide-y divide-gray-200">
             <thead>
               <tr className="border-b ">
-                <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                <th className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
                   CÔNG TY CỔ PHẦN THƯƠNG MẠI DỊCH VỤ
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                <th className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
                   Đơn giá
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                <th className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
                   Số lượng
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                <th className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
                   Thành tiền
                 </th>
               </tr>
@@ -59,12 +70,17 @@ const CartPage = () => {
                   <span className="text-sm font-medium text-gray-700 mr-4">
                     {calculatePrice()}
                   </span>
+                  {!isSmallScreen && (
+                    <span className="hidden lg:inline-block text-gray-400">
+                      đơn vị tính
+                    </span>
+                  )}
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
                   <div className="flex items-center justify-between px-2 py-1 bg-gray-200 rounded w-[70px]">
                     <button
                       className="text-sm font-medium text-gray-700 focus:outline-none"
-                      onClick={decreaseCount}
+                     onClick={decreaseCount}
                     >
                       -
                     </button>
@@ -83,6 +99,11 @@ const CartPage = () => {
                   <span className="text-sm font-medium text-gray-700 mr-4">
                     {calculatePrice()}
                   </span>
+                  {!isSmallScreen && (
+                    <span className="hidden lg:inline-block text-gray-400">
+                      đơn vị tính
+                    </span>
+                  )}
                 </td>
               </tr>
             </tbody>
@@ -90,28 +111,36 @@ const CartPage = () => {
         </div>
       </div>
 
-      <div className="col-span-1 mt-28 mr-16">
+      <div className="lg:col-span-1 mt-8 lg:mt-28 lg:mr-16">
         <div className="grid grid-rows-2">
-          <div className="bg-white border-2 rounded-lg p-4 shadow-2xl  row-span-1">
+          <div className="bg-white border-2 rounded-lg p-4 shadow-2xl row-span-1 lg:mb-8">
             <h2 className="text-lg font-bold mb-4">Khuyến mãi</h2>
             <span>Đơn hàng chưa đủ điều kiện</span>
           </div>
 
           <div className="bg-white border-2 rounded-lg p-4 shadow-2xl mt-8 row-span-1">
             <h2 className="text-lg font-bold mb-4">Thanh toán</h2>
-            <p className=" flex mb-1">
-              Tổng tạm tính
-              <span className=" ml-auto">$30.00</span>
+            <p className="flex mb-1">
+              Tạm tính
+              <span className="ml-auto">{calculatePrice()}</span>
             </p>
-            <p className=" flex mb-4">
-              Tổng tạm tính
-              <span className="text-blue-500 ml-auto font-bold">$30.00</span>
+            {!isSmallScreen && (
+              <p className="flex mb-4">
+                Giảm giá
+                <span className="text-green-500 ml-auto">-$0.00</span>
+              </p>
+            )}
+            <p className="flex mb-4">
+              Tổng cộng
+              <span className="text-blue-500 ml-auto font-bold">
+                ${calculatePrice()}
+              </span>
             </p>
             <button
               type="submit"
               className="group w-full flex justify-center py-2 px-4 text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 uppercase text-lg"
             >
-              thanh toán
+              Thanh toán
             </button>
           </div>
         </div>
