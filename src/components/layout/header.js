a3b0fbc114ba7e0bba7f60a7e816918208aae770
NguyenMinhTuan <minhdu238@gmail.com>
2023-05-29 10:19:43 +0700

    update

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -41,7 +41,7 @@ function Header() {
               )}
               {User.User && (
                 <button className="text-white dark:text-white cursor-pointer hover:bg-sky-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
-                  {JSON.parse(localStorage.getItem(Userkey)).email}
+                  {JSON.parse(localStorage.getItem(Userkey)).firstname +" "+ JSON.parse(localStorage.getItem(Userkey)).lastname}
                 </button>
               )}
               {User.User && (
