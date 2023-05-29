55985eaef28e0150e013c2815655076dd8df0bed
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-05-29 16:24:54 +0700

    update

--- src/components/cart/products.js
+++ src/components/cart/products.js
@@ -18,32 +18,41 @@ const ProductsPage = () => {
   }, []);
   
   return (
-    <div className="product-list">
-      <div className="product-grid">
+    <div className="product-list md:w-4/5 sm:w-full lg:w-3/4 xl:w-2/3 mx-auto">
+      <div className="product-grid grid grid-cols-1 md:grid-cols-5 gap-4 mt-8">
         {/* Hiển thị danh sách sản phẩm */}
         {products.map((product) => (
           <Link to={`product/${product.id}`}>
-            <div key={product.id} className="product-item">
-              <div className="product-image">
+            <div key={product.id} className="product-item col-span-1 border-2 bg-white">
+              <div className="product-image py-2 px-4 ">
                 {/* Hiển thị ảnh sản phẩm */}
-                <img
-                  // src={product.custom_attributes.find(attr => attr.attribute_code === 'image').value}
-                  src="http://192.168.1.9/magento2/pub/media/catalog/product/cache/80c6d82db34957c21ffe417663cf2776////m/o/modem_wifi_4_cong.jpg"
-                  alt={product.name}
-                />
+                <div className="flex justify-center items-center h-48 md:h-auto">
+                  <img
+                    // src={product.custom_attributes.find(attr => attr.attribute_code === 'image').value}
+                    src="http://192.168.1.9/magento2/pub/media/catalog/product/cache/80c6d82db34957c21ffe417663cf2776////m/o/modem_wifi_4_cong.jpg"
+                    alt={product.name}
+                    className="object-contain max-w-full h-full"
+                  />
+                </div>
               </div>
 
               {/* Hiển thị tên sản phẩm */}
-              <div className="product-name">{product.name}</div>
+              <div className="product-name mt-2 overflow-hidden mb-4 ml-4 uppercase line-clamp-3">
+                  {product.name}
+              </div>
 
               {/* Hiển thị giá sản phẩm */}
-              <div className="product-price">{product.price} đ</div>
+              <div className="product-price mt-8 mb-4 ml-4">
+                <span className="text-lg font-bold text-blue-900 ">
+                  {product.price}.000 <u>đ</u>{' '}
+                </span>
+              </div>
             </div>
           </Link>
         ))}
       </div>
 
-      <div className="flex justify-center items-centermt-8 mb-8">
+      <div className="flex justify-center items-center mt-16 mb-16">
         <Stack spacing={2}>
           <Pagination count={10} showFirstButton showLastButton />
           {/* <Pagination count={10} hidePrevButton hideNextButton /> */}
