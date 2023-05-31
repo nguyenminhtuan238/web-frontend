0cc5306f7cef801ebe1f4f526c288cbb7c3a2a96
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-05-31 08:29:42 +0700

    update

--- src/components/cart/products.js
+++ src/components/cart/products.js
@@ -4,6 +4,8 @@ import Stack from '@mui/material/Stack';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getpd } from '../../store/products';
+import { img } from '../../unilt/key';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 const ProductsPage = () => {
   const get = useSelector((state) => state.products);
   const dispatch = useDispatch();
@@ -12,14 +14,15 @@ const ProductsPage = () => {
   const getPD=()=>{
     
     dispatch(getpd())
+    // console.log(get.Product[0].custom_attributes.find(a=>{return a.attribute_code==='image'}).value)
   }
   getPD()
   }, [dispatch]);
   
   return (
     <div>
-    {get.isloading?<div class="flex justify-center items-center">
-  <div class="w-16 h-16 border-4 border-t-4 border-gray-200 rounded-full animate-spin"></div>
+    {get.isloading?<div className="flex justify-center items-center">
+  <div className="w-16 h-16 border-4 border-t-4 border-gray-200 rounded-full animate-spin"></div>
 </div>:
     <div className="product-list md:w-4/5 sm:w-full lg:w-3/4 xl:w-2/3 mx-auto">
       <div className="product-grid grid grid-cols-1 md:grid-cols-5 gap-4 mt-8">
@@ -31,27 +34,35 @@ const ProductsPage = () => {
                 {/* Hiển thị ảnh sản phẩm */}
                 <div className="flex justify-center items-center h-50 md:h-auto" >
                   <img
-                    // src={product.custom_attributes.find(attr => attr.attribute_code === 'image').value}
-                    src="http://192.168.1.9/magento2/pub/media/catalog/product/cache/80c6d82db34957c21ffe417663cf2776////m/o/modem_wifi_4_cong.jpg"
-                    alt={product.name}
-                    className="object-contain max-w-full h-full"
+                      src={img+product.custom_attributes.find(a=>{return a.attribute_code==='image'}).value}
+                      alt={product.name}
+                      className="object-contain max-w-full h-full"
                   />
                 </div>
               </div>
 
               {/* Hiển thị tên sản phẩm */}
               <div className="product-name mt-2 overflow-hidden mb-2 ml-4 uppercase line-clamp-3 min-h-[70px]">
-                  {product.name} 
+                  {product.name}
               </div>
 
               {/* Hiển thị giá sản phẩm */}
               <div className="product-price mt-2 mb-4 ml-4">
                 <span className="text-lg font-bold text-blue-900 ">
-                  {product.price} <u>đ</u>{' '}
+                  {product.price} <u>đ</u>{' '}                   
                 </span>
+                <div className="float-right mr-2 hover:bg-gray-300 rounded-full w-8 h-8 flex justify-center items-center">
+                  
+                  <Link to="/cart">
+                    <button>
+                      <AddShoppingCartIcon />
+                    </button>
+                    
+                  </Link>
+                </div>
               </div>
             </div>
-          </Link>
+          </Link> 
         ))}
       </div>
 
@@ -66,5 +77,4 @@ const ProductsPage = () => {
     </div>
   );
 };
-
 export default ProductsPage;
