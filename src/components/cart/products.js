c67204bc6d001186b1d2a567ac2e4bd7ad418e2c
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-05-31 09:21:58 +0700

    update

--- src/components/cart/products.js
+++ src/components/cart/products.js
@@ -28,46 +28,44 @@ const ProductsPage = () => {
       <div className="product-grid grid grid-cols-1 md:grid-cols-5 gap-4 mt-8">
         {/* Hiển thị danh sách sản phẩm */}
         {get.Product.map((product) =>  (
-          <Link to={`product/${product.id}`}  key={product.id}>
-            <div className="product-item col-span-1 border-2 bg-white">
-              <div className="product-image py-2 px-4 ">
-                {/* Hiển thị ảnh sản phẩm */}
-                <div className="flex justify-center items-center h-50 md:h-auto" >
-                  <img
-                      src={img+product.custom_attributes.find(a=>{return a.attribute_code==='image'}).value}
-                      alt={product.name}
-                      className="object-contain max-w-full h-full"
-                  />
+          <div key={product.id}> 
+            <Link to={`product/${product.id}`}  >
+              <div className="product-item col-span-1 border-2 bg-white">
+                <div className="product-image py-2 px-4 ">
+                  {/* Hiển thị ảnh sản phẩm */}
+                  <div className="flex justify-center items-center h-50 md:h-auto" >
+                    <img
+                        src={img+product.custom_attributes.find(a=>{return a.attribute_code==='image'}).value}
+                        alt={product.name}
+                        className="object-contain max-w-full h-full"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              {/* Hiển thị tên sản phẩm */}
-              <div className="product-name mt-2 overflow-hidden mb-2 ml-4 uppercase line-clamp-3 min-h-[70px]">
-                  {product.name}
-              </div>
+                {/* Hiển thị tên sản phẩm */}
+                <div className="product-name mt-2 overflow-hidden mb-2 ml-4 uppercase line-clamp-3 min-h-[70px]">
+                    {product.name}
+                </div>
 
-              {/* Hiển thị giá sản phẩm */}
-              <div className="product-price mt-2 mb-4 ml-4">
-                <span className="text-lg font-bold text-blue-900 ">
-                  {product.price} <u>đ</u>{' '}                   
-                </span>
-                <div className="float-right mr-2 hover:bg-gray-300 rounded-full w-8 h-8 flex justify-center items-center">
-                  
-                  <Link to="/cart">
+                {/* Hiển thị giá sản phẩm */}
+                <div className="product-price mt-2 mb-4 ml-4">
+                  <span className="text-lg font-bold text-blue-900 ">
+                    {product.price} <u>đ</u>{' '}                   
+                  </span>
+                  <div className="float-right mr-2 hover:bg-gray-300 rounded-full w-8 h-8 flex justify-center items-center">
                     <button>
                       <AddShoppingCartIcon />
                     </button>
-                    
-                  </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link> 
+            </Link> 
+          </div>
         ))}
       </div>
 
       <div className="flex justify-center items-center mt-16 mb-16">
-        <Stack spacing={2}>
+        <Stack spacing={1}>
           <Pagination count={10} showFirstButton showLastButton />
           {/* <Pagination count={10} hidePrevButton hideNextButton /> */}
         </Stack>
