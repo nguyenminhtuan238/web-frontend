1ac36e9743c13b2b0d15e5fa300ab190831b0fde
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-06-20 15:33:40 +0700

    update

--- src/components/hoadon.js
+++ src/components/hoadon.js
@@ -22,69 +22,6 @@ function Hoadon() {
     setSelectedRowIndex(index);
   };
 
-<<<<<<< HEAD
-    useEffect(() => {
-      async function fetchOrders() {
-        try {
-          // Lấy token từ localStorage
-          const token = localStorage.getItem(Storagekey);
-          // Gửi yêu cầu HTTP GET đến endpoint trên server với Authorization header chứa token
-          const response = await axios.get('http://192.168.1.9:5000/order/list/', {
-            headers: { Authorization: `Bearer ${token}` },
-          });
-          console.log(response.data);
-          // Cập nhật state với dữ liệu trả về từ server
-          setOrders(response.data.orders);
-        } catch (error) {
-          console.error(error);
-        }
-      }
-      fetchOrders();
-    }, []);
-  
-    const handleRowClick = (index) => {
-      setSelectedRowIndex(index);
-    };
-
-    return (
-      <div className="w-full py-5 flex flex-col">
-        <div className="overflow-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <h1 className="text-center mb-8 uppercase font-bold">Hóa Đơn Của Bạn</h1>
-              <table className="w-full text-left text-sm font-light w-5 overflow-hidden ">
-                <thead className="bg-gray-700 text-white border-b font-medium dark:border-neutral-500">
-                  <tr className="px-4 py-6 text-center ">
-                    <th scope="col" className="px-6 py-4">
-                      #
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                        Tài khoản
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                        Địa chỉ
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                        Số điện thoại
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                        Tên sản phẩm
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                         Số lượng
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                         Giá sản phẩm
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                         Tổng tiền
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                         Ngày lập hóa đơn
-                    </th>
-                  </tr>
-                </thead>
-=======
   return (
     <div className="py-5 flex flex-col my-5">
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -126,7 +63,6 @@ function Hoadon() {
                 </tr>
               </thead>
               {get.isloading ? (
->>>>>>> 956da3470ad42de3a2d6da512d13ca5dff085bb1
                 <tbody>
                   <tr>
                     <td colSpan="9" className="px-4 py-2">
